72479adeae68aa7cead54cd72fdef4046110e557
Piscoi Andrei <piscoiandrei27@gmail.com>
2022-03-07 19:29:50 +0200

    Destructors

---
+++ cpp-1/destructors.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+class Entity
+{
+private:
+	int x;
+public:
+	Entity() {}
+	Entity(int value) { x = value; }
+
+	~Entity()
+	{
+		// Usually implemented for memory management
+	}
+
+};
+
+int main()
+{
+
+}
